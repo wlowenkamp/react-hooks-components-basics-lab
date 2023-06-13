0f08ab24935cee8da129008247369c83a592feae
Will Lowenkamp <lowenkamp97@gmail.com>
2023-06-13 14:34:19 -0400

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,19 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About (){
+  return ( <div id = "about">
+    <h1>about</h1>
+  </div>)
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
